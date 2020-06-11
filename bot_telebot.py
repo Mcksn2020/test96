1ff08d13dedbb0ec2340b1ee7ec4c2c716a660f4
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-11 14:19:05 +0300

    Update bot_telebot.py

--- bot_telebot.py
+++ bot_telebot.py
@@ -1,11 +1,15 @@
 import telebot
-from telebot import apihelper
-apihelper.proxy={'https':'socks5h://userproxy:password@proxy_address:port'}
+
 bot=telebot.TeleBot('1252740921:AAHHgCl3AZVc--cK-Lz1P9uxZraponCpmZc')
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+        bot.send_message(message.chat.id,'Привет, это ПАПА')
+        
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text=='Привет':
-         bot.send_message(message.from_user.id,'Привет, это ПАПА, кагдила?')
+         bot.send_message(message.from_user.id,'Привет, кагдила?')
     else:
         bot.send_message(message.from_user.id,'Ну да')
 bot.polling(none_stop=True,interval=0)
