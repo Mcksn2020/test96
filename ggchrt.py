8841a5eff7d237874f6e8c3bb83fec02435b3b1c
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-17 14:27:48 +0300

    Update ggchrt.py

--- ggchrt.py
+++ ggchrt.py
@@ -1,3 +1,14 @@
-import requests
+#'https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
+#здесь строим график
 
-gc='https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
+
+gc0='https://chart.googleapis.com/chart?'
+gc_helwor='chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
+
+gc=gc0+gc_helwor
+
+g_cht='1xy'
+g_chd='t:10.5,20.56|7.5,33.8'
+
+
+
