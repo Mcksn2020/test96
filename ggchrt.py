27fbeddf138e53ae6dccfd1849be4f17c5b7dddc
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-17 14:32:41 +0300

    Update ggchrt.py

--- ggchrt.py
+++ ggchrt.py
@@ -5,10 +5,11 @@
 gc0='https://chart.googleapis.com/chart?'
 gc_helwor='chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
 
-gc=gc0+gc_helwor
+#gc=gc0+gc_helwor
 
-g_cht='1xy'
-g_chd='t:10.5,20.56|7.5,33.8'
+g_cht='cht=1xy'
+g_chd='chd=t:10.5,20.56|7.5,33.8'
 
+gc=gc0+g_cht+'&'+g_chd
 
-
+print(gc)
