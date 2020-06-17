83afa32c1b98e2589c7576f4e0e1288c23862629
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-17 13:38:59 +0300

    Update bot_telebot.py

--- bot_telebot.py
+++ bot_telebot.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+import ggchrt
 bot=telebot.TeleBot(config.token)
 cnt=0
 
@@ -13,10 +14,9 @@ def get_text_messages(message):
     if message.text=='Привет':
          bot.send_message(message.from_user.id,'Привет, кагдила?')
     elif message.text=="Photo":
-         bot.send_document(message.from_user.id,'https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World')
+         bot.send_document(message.from_user.id,ggchrt.gs)
     elif message.text=='Document':
-         bot.send_document(message.from_user.id,'https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World')
-                         
+         bot.send_document(message.from_user.id,ggchrt.gs)       
     else:
         bot.send_message(message.from_user.id,'Ну да')
 bot.polling(none_stop=True,interval=0)
