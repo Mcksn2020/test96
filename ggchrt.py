2875e8a2c37b6be2227180b6051ddc108ed48cf7
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-17 15:23:36 +0300

    Update ggchrt.py

--- ggchrt.py
+++ ggchrt.py
@@ -7,12 +7,12 @@ gc_helwor='chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
 
 #gc=gc0+gc_helwor
 
-g_cht='cht=p3' #'cht=1xy'
-g_chd='chd=t:60,40' #|7,33'
+g_cht='cht=lxy'
+g_chd='chd=t:60,40|7,33'
 g_chs='chs=250x100'
 
 #g_chl='Hell|Wor'
 
 gc=gc0+g_cht+'&'+g_chd+'&'+g_chs
 
-print(gc)
+#print(gc)
