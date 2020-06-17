5afe24de413bebfa727607976f36ab7e0df24dbb
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-17 20:03:15 +0300

    Update ggchrt.py

--- ggchrt.py
+++ ggchrt.py
@@ -1,11 +1,19 @@
 #'https://chart.googleapis.com/chart?chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
 #здесь строим график
 #все взято отсюда https://developers.google.com/chart/image/docs/gallery/line_charts
+import point_calc
 
-gc0='https://chart.googleapis.com/chart?'
-gc_helwor='chs=250x100&chd=t:60,40&cht=p3&chl=Hello|World'
+x11=str(point_calc.x1)
+x12=str(point_calc.x2)
+x13=str(point_calc.x3)
+x_ii=str(point_calc.x_init)
+x_ee=str(point_calc.x_end)
 
-#gc=gc0+gc_helwor
+
+gc='https://chart.googleapis.com/chart?'
+
+
+
 
 g_cht='cht=lxy'                #линия
 g_chd='chd=t:10.5,40|7,33'    #точки
@@ -16,11 +24,13 @@ g_chl='chl=ОПЦИОНЫ'                 # название
 g_chc='chco=00FF00'              # цвет линии
 g_chttl='chtt=Опционный+портфель'   #название
 g_chtts='chts=FF0000,20,c'         #шрифт названия
-g_chxt='chxt=x,y,r,t'
-g_grid='chg=10,20'
+g_chxt='chxt=x,y,r,t'             #оси
+g_grid='chg=10,20'              #шаг сетки
+g_chxr='0,x_ii,x_ee,str(point_calc.x_st)'  #цифры по оси Х
 
-gc=gc0+g_cht+'&'+g_chd+'&'+g_chs+'&'+g_chc+'&'+g_chttl+'&'+g_chtts
-gc=gc+'&'+g_chxt+'&'+g_grid
 
-#print(gc)
+gc=gc+g_cht+'&'+g_chd+'&'+g_chs+'&'+g_chc+'&'+g_chttl+'&'+g_chtts
+gc=gc+'&'+g_chxt+'&'+g_grid+'&'+ g_chxr
+
+print(gc)
 
