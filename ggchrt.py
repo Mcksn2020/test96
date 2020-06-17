a77d3aa35e05612ddec124c523fe27ea06a48b55
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-17 18:37:59 +0300

    Update ggchrt.py

--- ggchrt.py
+++ ggchrt.py
@@ -11,7 +11,7 @@ g_cht='cht=lxy'                #линия
 g_chd='chd=t:10.5,40|7,33'    #точки
 #g_chs1='chs=250x100'
 #g_chs2='chs=250x250'
-g_chs='chs=300x300'            #   размер
+g_chs='chs=400x400'            #   размер
 g_chl='chl=ОПЦИОНЫ'                 # название
 g_chc='chco=00FF00'              # цвет линии
 g_chttl='chtt=Опционный+портфель'   #название
@@ -19,7 +19,7 @@ g_chtts='chts=FF0000,20,c'         #шрифт названия
 g_chxt='chxt=x,y,r,t'
 g_grid='chg=10,20'
 
-gc=gc0+g_cht+'&'+g_chd+'&'+g_chs+'&'+g_chl+'&'+g_chc+'&'+g_chttl+'&'+g_chtts
+gc=gc0+g_cht+'&'+g_chd+'&'+g_chs+'&'+g_chc+'&'+g_chttl+'&'+g_chtts
 gc=gc+'&'+g_chxt+'&'+g_grid
 
 #print(gc)
