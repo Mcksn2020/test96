270bbcdb66133180a14419781c04173a09e6358a
Mcksn2020 <66720094+Mcksn2020@users.noreply.github.com>
2020-06-18 19:14:24 +0300

    Update bot_telebot.py

--- bot_telebot.py
+++ bot_telebot.py
@@ -7,14 +7,14 @@ cnt_d=0
 
 bot=telebot.TeleBot(config.token)
 
-keyboard0 = types.ReplyKeyboardMarkup()
-keyboard0.row('Voc', 'Standard Srategy') #,'Build You Strategy','Quiz Strategy')
+#keyboard0 = types.ReplyKeyboardMarkup()
+#keyboard0.row('Voc', 'Standard Srategy') #,'Build You Strategy','Quiz Strategy')
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-        cnt_d+=1
-        bot.send_message(message.chat.id,'Привет, это ПАПА'
-        ,reply_markup=keyboard0)
+        #cnt_d+=1
+        bot.send_message(message.chat.id,'Привет, это ПАПА')
+        #,reply_markup=keyboard0)
         
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
